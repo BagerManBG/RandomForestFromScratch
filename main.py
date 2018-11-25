0bc41b2e878278d50771516f63d9e6c807da514e
ZKaramanolov <>
2018-11-26 01:25:10 +0200

    Reandom forest Implementation added

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ RANDOM_SEED = 42
 
 np.random.seed(RANDOM_SEED)
 
-train_csv = 'https://raw.githubusercontent.com/Data-Science-FMI/ml-from-scratch-2019/master/data/house_prices_train.csv'
-test_csv = 'https://raw.githubusercontent.com/Data-Science-FMI/ml-from-scratch-2019/master/data/house_prices_test.csv'
+train_csv = 'train/house_prices_train.csv'
+test_csv = 'train/house_prices_test.csv'
 
 df_train = pd.read_csv(train_csv)
 df_test = pd.read_csv(test_csv)
@@ -55,6 +55,7 @@ class Node:
     self.x = x
     self.y = y
     self.idxs = idxs
+    self.idxs_native = np.array(np.arange(len(self.idxs)))
     self.min_leaf = min_leaf
     self.row_count = len(idxs)
     self.col_count = x.shape[1]
@@ -72,11 +73,13 @@ class Node:
     self.rhs = Node(self.x, self.y, self.idxs[rhs])
 
   def find_better_split(self, var_idx):
-    x, y = self.x.values[self.idxs, var_idx], self.y[self.idxs]
+    x, y = self.x.values[self.idxs_native, var_idx], self.y[self.idxs]
 
     for r in range(self.row_count):
       lhs = x <= x[r]  # any value in x that is less or equal than this value
       rhs = x > x[r]  # any value in x that is greater than this value
+
+      # lhs/rhs = [1,0,0,1,1,0,1,1,0,1]
 
       if rhs.sum() < self.min_leaf or lhs.sum() < self.min_leaf: continue
 
@@ -95,7 +98,7 @@ class Node:
 
   @property
   def split_col(self):
-    return self.x.values[self.idxs, self.var_idx]
+    return self.x.values[self.idxs_native, self.var_idx]
 
   @property
   def is_leaf(self):
@@ -117,22 +120,52 @@ class Node:
 
 
 class DecisionTreeRegressor:
-  def fit(self, X, y, min_leaf=5):
-    self.dtree = Node(X, y, np.array(np.arange(len(y))), min_leaf)
+  def fit(self, X, y, idxs, min_leaf=5):
+    self.dtree = Node(X, y, idxs, min_leaf)
     return self
 
   def predict(self, X):
     return self.dtree.predict(X.values)
 
+# Random Forest
+class RandomForest():
+    def __init__(self, x, y, n_trees, sample_sz, depth=10, min_leaf=5):
+        self.x = x
+        self.y = y
+        self.sample_sz = sample_sz
+        self.depth = depth
+        self.min_leaf = min_leaf
+        self.trees = [self.create_tree() for i in range(n_trees)]
 
-regressor = DecisionTreeRegressor().fit(X, y)
-preds = regressor.predict(X)
+    def create_tree(self):
+        idxs = np.random.permutation(len(self.y))[:self.sample_sz]
+        tree = DecisionTreeRegressor()
+        x = self.x.iloc[idxs]
+        y = self.y[idxs]
+
+        # print(x.shape)
+        # print(len(idxs))
+        # print(x)
+        tree.fit(x, y, idxs, self.min_leaf)
+        return tree
+        
+    def predict(self, x):
+        return np.mean([t.predict(x) for t in self.trees], axis=0)
+
+forest = RandomForest(X, y, n_trees=150, sample_sz=399, depth=5, min_leaf=5)
+preds = forest.predict(X)
+
+# regressor = DecisionTreeRegressor().fit(X, y)
+# preds = regressor.predict(X)
 
 metrics.r2_score(y, preds)
-rmse(preds, y)
+err = rmse(preds, y)
+
+print(err)
 
 X_test = df_test[['OverallQual', 'GrLivArea', 'GarageCars']]
-pred_test = regressor.predict(X_test)
+# pred_test = regressor.predict(X_test)
+pred_test = forest.predict(X_test)
 submission = pd.DataFrame({'Id': df_test.Id, 'SalePrice': pred_test})
 
 script_dir = os.path.abspath(os.path.dirname(sys.argv[0]) or '.')
